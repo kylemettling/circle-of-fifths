f4dd1ba0b62c4ab51caeb319469d52dd8f7b78ee
WON-DER <wonderfilledyt@gmail.com>
2022-10-05 18:32:32 -0700

    2nd commit

---
+++ components/slice.js
@@ -0,0 +1,5 @@
+const Slice = () => {
+	return <div>Hello!</div>
+}
+
+export default Slice
---
+++ components/slices.js
@@ -0,0 +1,8 @@
+import Slice from './slice'
+
+const Slices = () => {
+	const roots = Array.from(12)
+	return <div>{JSON.stringify(roots)}</div>
+}
+
+export default Slices

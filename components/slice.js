8474c3d2df96b9b7f282b797772a9be52ac22c28
WON-DER <wonderfilledyt@gmail.com>
2022-10-11 17:28:10 -0700

    EOD push

--- components/slice.js
+++ components/slice.js
@@ -1,5 +1,20 @@
-const Slice = () => {
-	return <div>Hello!</div>
+import Image from 'next/image'
+import SignatureSVG from './SignatureSVG'
+const Slice = ({ item }) => {
+	return (
+		<div className='slice'>
+			<span>Hello!</span>
+			<pre>{JSON.stringify(item, null, 4)}</pre>
+			<SignatureSVG />
+			{/* <Image
+				alt={item.major}
+				src={`/images/${item.accidentals.count + item.accidentals.type}.svg`}
+				height={'100px'}
+				width={'300px'}
+				style={{ stroke: 'red', fill: 'blue' }}
+			></Image> */}
+		</div>
+	)
 }
 
 export default Slice
--- components/slices.js
+++ components/slices.js
@@ -1,37 +1,37 @@
-import Slice from "./slice";
-import signatures from "../lib/state/signatures";
-import Image from "next/image";
-import { useState } from "react";
+import Slice from './slice'
+import signatures from '../lib/state/signatures'
+import Image from 'next/image'
+import { useState } from 'react'
 
 const Slices = () => {
-  const keys = [...Array(12).keys()].map((idx) => {
-    const item = signatures[idx];
-    const newItem = {
-      index: idx,
-      image: `${
-        item.accidentals ? item.accidentals.count + item.accidentals.type : null
-      }`,
-      ...item,
-    };
-    return newItem;
-  });
+	const keys = [...Array(12).keys()].map((idx) => {
+		const item = signatures[idx]
+		const newItem = {
+			index: idx,
+			image: `${
+				item.accidentals ? item.accidentals.count + item.accidentals.type : null
+			}`,
+			...item,
+		}
+		return newItem
+	})
 
-  //   const items = keys.map(async (item) => {});
-  return (
-    <div>
-      {/* {items} */}
-      {/* {keys.map((item) => {
-        return (
-          <Image
-            src={`/images/${item.image}.svg`}
-            alt="test"
-            height={"300px"}
-            width={"300px"} 
-          />
-        );
-      })} */}
-    </div>
-  );
-};
+	return (
+		<div>
+			{/* {items} */}
+			{keys.map((item) => {
+				return <Slice key={item.index} item={item} />
+				// return (
+				// 	<Image
+				// 		src={`/images/${item.image}.svg`}
+				// 		key={item.index}
+				// 		alt='test'
+				// 		height={'300px'}
+				// 		width={'300px'}
+				// 	/>
+			})}
+		</div>
+	)
+}
 
-export default Slices;
+export default Slices

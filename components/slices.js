c95eabbfe29f9fa7c3fedf7492328cb64ca3632a
kylemettling <wonderfilledyt@gmail.com>
2022-10-12 16:53:49 -0700

    styling edits

--- components/slices.js
+++ components/slices.js
@@ -1,37 +1,36 @@
-import Slice from './slice'
-import signatures from '../lib/state/signatures'
-import Image from 'next/image'
-import { useState } from 'react'
+import Slice from "./slice";
+import signatures from "../lib/state/signatures";
+import Image from "next/image";
 
 const Slices = () => {
-	const keys = [...Array(12).keys()].map((idx) => {
-		const item = signatures[idx]
-		const newItem = {
-			index: idx,
-			image: `${
-				item.accidentals ? item.accidentals.count + item.accidentals.type : null
-			}`,
-			...item,
-		}
-		return newItem
-	})
+  const keys = [...Array(12).keys()].map((idx) => {
+    const item = signatures[idx];
+    const newItem = {
+      index: idx,
+      image: `${
+        item.accidentals ? item.accidentals.count + item.accidentals.type : null
+      }`,
+      ...item,
+    };
+    return newItem;
+  });
 
-	return (
-		<div>
-			{/* {items} */}
-			{keys.map((item) => {
-				return <Slice key={item.index} item={item} />
-				// return (
-				// 	<Image
-				// 		src={`/images/${item.image}.svg`}
-				// 		key={item.index}
-				// 		alt='test'
-				// 		height={'300px'}
-				// 		width={'300px'}
-				// 	/>
-			})}
-		</div>
-	)
-}
+  return (
+    <div>
+      {/* {items} */}
+      {keys.map((item) => {
+        return <Slice key={item.index} item={item} />;
+        // return (
+        // 	<Image
+        // 		src={`/images/${item.image}.svg`}
+        // 		key={item.index}
+        // 		alt='test'
+        // 		height={'300px'}
+        // 		width={'300px'}
+        // 	/>
+      })}
+    </div>
+  );
+};
 
-export default Slices
+export default Slices;

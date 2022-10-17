ae4fd593acad2322794cbc01b88ab76ad28e1cf9
WON-DER <wonderfilledyt@gmail.com>
2022-10-16 18:06:25 -0700

    EOD push

--- components/slices.js
+++ components/slices.js
@@ -1,27 +1,27 @@
-import Slice from "./slice";
-import signatures from "../lib/state/signatures";
-import Image from "next/image";
+import Slice from './slice'
+import signatures from '../lib/state/signatures'
+import Image from 'next/image'
 
 const Slices = () => {
-  const keys = [...Array(signatures.length).keys()].map((sig, idx) => {
-    const item = signatures[idx];
-    const newItem = {
-      index: idx,
-      position: idx,
-      image: `${
-        item.accidentals ? item.accidentals.count + item.accidentals.type : null
-      }`,
-      ...item,
-    };
-    return newItem;
-  });
-  return (
-    <div className="slices">
-      {keys.map((item) => {
-        return <Slice key={item.index} item={item} />;
-      })}
-    </div>
-  );
-};
+	const keys = [...Array(signatures.length).keys()].map((sig, idx) => {
+		const item = signatures[idx]
+		const newItem = {
+			index: idx,
+			position: idx,
+			image: `${
+				item.accidentals ? item.accidentals.count + item.accidentals.type : null
+			}`,
+			...item,
+		}
+		return newItem
+	})
+	return (
+		<div className='slices'>
+			{keys.map((item) => {
+				return <Slice key={item.index} item={item} />
+			})}
+		</div>
+	)
+}
 
-export default Slices;
+export default Slices
